5459eb12fe6444997a31b4e98fe635da778a766e
wang0nya <kwangonya@gmail.com>
2018-06-26 09:19:59 +0300

    convert function

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ getCurrencies.open('GET', 'https://free.currencyconverterapi.com/api/v5/currenci
 getCurrencies.onload = function () {
 
     // Begin accessing JSON data here
-    const data = JSON.parse(this.response);
+    let data = JSON.parse(this.response);
     if (getCurrencies.status >= 200 && getCurrencies.status < 400) {
         for (const currency in data.results) {
-            document.getElementById("currencyFromList").innerHTML += '<option>' + currency + '</option>';
-            document.getElementById("currencyToList").innerHTML += '<option>' + currency + '</option>';
+            document.getElementById("currencyFromList").innerHTML += '<option value=`this.currency`>' + currency + '</option>';
+            document.getElementById("currencyToList").innerHTML += '<option value="PHP">' + currency + '</option>';
         }
     } else {
         console.log('error');
@@ -17,3 +17,26 @@ getCurrencies.onload = function () {
 };
 
 getCurrencies.send();
+
+// convert selected currencies
+const convertCurrencies = new XMLHttpRequest();
+
+function convert() {
+    const from = document.getElementById("currencyFromList");
+    const fromValue = from.options[from.selectedIndex].text;
+    const to = document.getElementById("currencyToList");
+    const toValue = to.options[to.selectedIndex].text;
+    convertCurrencies.open('GET', `https://free.currencyconverterapi.com/api/v5/convert?q=${fromValue}_${toValue}&compact=y`, true);
+    convertCurrencies.onload = function () {
+        // Begin accessing JSON data here
+        let data = JSON.parse(this.response);
+        if (convertCurrencies.status >= 200 && convertCurrencies.status < 400) {
+            console.log(data);
+
+        } else {
+            console.log('error');
+        }
+    };
+
+    convertCurrencies.send()
+}
