b8852431e801ccd3f6eea5a195c867fc86ba2a5b
wang0nya <kwangonya@gmail.com>
2018-06-26 11:40:19 +0300

    es6-ify

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ getCurrencies.onload = function () {
     let data = JSON.parse(this.response);
     if (getCurrencies.status >= 200 && getCurrencies.status < 400) {
         for (const currency in data.results) {
-            document.getElementById("currencyFromList").innerHTML += '<option>' + currency + '</option>';
-            document.getElementById("currencyToList").innerHTML += '<option>' + currency + '</option>';
+            document.getElementById("currencyFromList").innerHTML += `<option>${currency}</option>`;
+            document.getElementById("currencyToList").innerHTML += `<option>${currency}</option>`;
         }
     } else {
         console.log('error');
@@ -27,7 +27,7 @@ function convert() {
     let to = document.getElementById("currencyToList");
     to = to.options[to.selectedIndex].text;
     const amount = document.getElementById("amountFrom").value;
-    const query = from + '_' + to;
+    const query = `${from}_${to}`;
     convertCurrencies.open('GET', `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`, true);
     convertCurrencies.onload = function () {
         // Begin accessing JSON data here
