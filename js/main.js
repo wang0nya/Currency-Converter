ead83c796f8fc18eef22ed613e0d6da3358cd80b
wang0nya <kwangonya@gmail.com>
2018-06-28 21:57:03 +0300

    use fetch api instead of XMLHttpRequest

--- js/main.js
+++ js/main.js
@@ -12,28 +12,18 @@ if ('serviceWorker' in navigator) {
 }
 
 // get all currencies from API
-const getCurrencies = new XMLHttpRequest();
-
-getCurrencies.open('GET', 'https://free.currencyconverterapi.com/api/v5/currencies', true);
-getCurrencies.onload = function () {
-
-    // Begin accessing JSON data here
-    let data = JSON.parse(this.response);
-    if (getCurrencies.status >= 200 && getCurrencies.status < 400) {
-        for (const currency in data.results) {
+fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(currencies) {
+        for (const currency in currencies.results) {
             document.getElementById("currencyFromList").innerHTML += `<option>${currency}</option>`;
             document.getElementById("currencyToList").innerHTML += `<option>${currency}</option>`;
         }
-    } else {
-        console.log('error');
-    }
-};
-
-getCurrencies.send();
+    });
 
 // convert selected currencies
-const convertCurrencies = new XMLHttpRequest();
-
 function convert() {
     let from = document.getElementById("currencyFromList");
     from = from.options[from.selectedIndex].text;
@@ -41,24 +31,13 @@ function convert() {
     to = to.options[to.selectedIndex].text;
     const amount = document.getElementById("amountFrom").value;
     const query = `${from}_${to}`;
-    convertCurrencies.open('GET', `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`, true);
-    convertCurrencies.onload = function () {
-        // Begin accessing JSON data here
-        let data = JSON.parse(this.response);
-        const rate = data[query];
-        // save data in idb
-        idbKeyval.set(query, rate)
-            .then(() => console.log('It worked!'))
-            .catch(err => console.log('It failed!', err));
-        // if data exists in idb, get from there
-        idbKeyval.get(query);
-        if (convertCurrencies.status >= 200 && convertCurrencies.status < 400) {
-            const result =  amount * rate;
+    fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (rate) {
+            const rateVal = rate[query];
+            const result =  amount * rateVal;
             document.getElementById("amountTo").value = result.toFixed(2);
-        } else {
-            console.log('error');
-        }
-    };
-
-    convertCurrencies.send()
+        });
 }
