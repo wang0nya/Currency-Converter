d711276271a8bfad2fa4325cb2c09d1d65e3347d
wang0nya <kwangonya@gmail.com>
2018-06-29 15:46:39 +0300

    add network listener

--- js/main.js
+++ js/main.js
@@ -85,3 +85,30 @@ function convert() {
         }
     }
 }
+
+// network listener
+window.addEventListener('load', () => {
+    const status = document.getElementById("status");
+
+    function updateOnlineStatus(event) {
+        const condition = navigator.onLine ? "online" : "offline";
+
+        status.className = condition;
+        if (condition === 'offline') {
+            status.innerHTML = `<div class="alert alert-warning alert-dismissible fade show text-center" role="alert">
+                                    You've gone ${condition}
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>`;
+        } else if (condition === 'online') {
+            // No need for an alert if user is online already. Everything will be working just fine
+            // Remove offline alert
+            status.innerHTML = '';
+        }
+
+    }
+
+    window.addEventListener('online',  updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+});
